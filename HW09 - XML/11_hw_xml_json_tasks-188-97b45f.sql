551d7196a851259d110046df211ed43ad09f76a9
Elena <evaigell@mail.ru>
2022-05-30 10:54:33 +0300

    Вопрос объединение с временной таблицей

---
+++ HW09 - XML/11_hw_xml_json_tasks-188-97b45f.sql
@@ -0,0 +1,149 @@
+
+/*
+Домашнее задание по курсу MS SQL Server Developer в OTUS.
+
+Занятие "08 - Выборки из XML и JSON полей".
+
+Задания выполняются с использованием базы данных WideWorldImporters.
+
+Бэкап БД можно скачать отсюда:
+https://github.com/Microsoft/sql-server-samples/releases/tag/wide-world-importers-v1.0
+Нужен WideWorldImporters-Full.bak
+
+Описание WideWorldImporters от Microsoft:
+* https://docs.microsoft.com/ru-ru/sql/samples/wide-world-importers-what-is
+* https://docs.microsoft.com/ru-ru/sql/samples/wide-world-importers-oltp-database-catalog
+*/
+
+-- ---------------------------------------------------------------------------
+-- Задание - написать выборки для получения указанных ниже данных.
+-- ---------------------------------------------------------------------------
+
+USE WideWorldImporters
+
+/*
+Примечания к заданиям 1, 2:
+* Если с выгрузкой в файл будут проблемы, то можно сделать просто SELECT c результатом в виде XML. 
+* Если у вас в проекте предусмотрен экспорт/импорт в XML, то можете взять свой XML и свои таблицы.
+* Если с этим XML вам будет скучно, то можете взять любые открытые данные и импортировать их в таблицы (например, с https://data.gov.ru).
+* Пример экспорта/импорта в файл https://docs.microsoft.com/en-us/sql/relational-databases/import-export/examples-of-bulk-import-and-export-of-xml-documents-sql-server
+*/
+
+
+/*
+1. В личном кабинете есть файл StockItems.xml.
+Это данные из таблицы Warehouse.StockItems.
+Преобразовать эти данные в плоскую таблицу с полями, аналогичными Warehouse.StockItems.
+Поля: StockItemName, SupplierID, UnitPackageID, OuterPackageID, QuantityPerOuter, TypicalWeightPerUnit, LeadTimeDays, IsChillerStock, TaxRate, 
+UnitPrice 
+
+Загрузить эти данные в таблицу Warehouse.StockItems: 
+существующие записи в таблице обновить, отсутствующие добавить (сопоставлять записи по полю StockItemName). 
+
+Сделать два варианта: с помощью OPENXML и через XQuery.
+*/
+
+
+--С помощью OPENXML
+DECLARE @xmlDocument  xml
+
+-- Считываем XML-файл в переменную
+-- !!! измените путь к XML-файлу
+SELECT @xmlDocument = BulkColumn
+FROM OPENROWSET
+(BULK 'D:\otus\11_StockItems-188-1fb5df.xml', SINGLE_CLOB) as data 
+
+-- Проверяем, что в @xmlDocument
+--SELECT @xmlDocument as [@xmlDocument]
+
+DECLARE @docHandle int
+EXEC sp_xml_preparedocument @docHandle OUTPUT, @xmlDocument
+
+---- можно вставить результат в таблицу
+DROP TABLE IF EXISTS #StockItem
+
+CREATE TABLE #StockItem(
+	[StockItemName]        nvarchar(100)  ,
+	[SupplierID]           int            ,
+	[UnitPackageID]        int            ,
+	[OuterPackageID]       int            ,
+	[QuantityPerOuter]     int            ,
+	[TypicalWeightPerUnit] decimal(18,3)  ,
+	[LeadTimeDays]         int            ,
+	[IsChillerStock]       bit            ,
+	[TaxRate]              decimal(18,3)  ,
+	[UnitPrice]            decimal(18,2) 
+)
+
+INSERT INTO #StockItem
+SELECT *
+FROM OPENXML(@docHandle, N'/StockItems/Item')
+WITH ( 
+	[StockItemName]        nvarchar(100)  '@Name',
+	[SupplierID]           int            'SupplierID',
+	[UnitPackageID]        int            'Package/UnitPackageID',
+	[OuterPackageID]       int            'Package/OuterPackageID',
+	[QuantityPerOuter]     int            'Package/QuantityPerOuter',
+	[TypicalWeightPerUnit] decimal(18,3)  'Package/TypicalWeightPerUnit',
+	[LeadTimeDays]         int            'LeadTimeDays',
+	[IsChillerStock]       bit            'IsChillerStock',
+	[TaxRate]              decimal(18,3)  'TaxRate',
+	[UnitPrice]            decimal(18,2)  'UnitPrice')
+
+-- Надо удалить handle
+EXEC sp_xml_removedocument @docHandle
+
+SELECT * FROM #StockItem
+
+
+SELECT wS.* FROM Warehouse.StockItems wS
+LEFT JOIN #StockItem SI ON wS.StockItemName = SI.StockItemName
+
+
+
+
+
+
+
+
+DROP TABLE IF EXISTS #StockItem
+
+/*
+2. Выгрузить данные из таблицы StockItems в такой же xml-файл, как StockItems.xml
+*/
+
+напишите здесь свое решение
+
+
+/*
+3. В таблице Warehouse.StockItems в колонке CustomFields есть данные в JSON.
+Написать SELECT для вывода:
+- StockItemID
+- StockItemName
+- CountryOfManufacture (из CustomFields)
+- FirstTag (из поля CustomFields, первое значение из массива Tags)
+*/
+
+напишите здесь свое решение
+
+/*
+4. Найти в StockItems строки, где есть тэг "Vintage".
+Вывести: 
+- StockItemID
+- StockItemName
+- (опционально) все теги (из CustomFields) через запятую в одном поле
+
+Тэги искать в поле CustomFields, а не в Tags.
+Запрос написать через функции работы с JSON.
+Для поиска использовать равенство, использовать LIKE запрещено.
+
+Должно быть в таком виде:
+... where ... = 'Vintage'
+
+Так принято не будет:
+... where ... Tags like '%Vintage%'
+... where ... CustomFields like '%Vintage%' 
+*/
+
+
+напишите здесь свое решение
